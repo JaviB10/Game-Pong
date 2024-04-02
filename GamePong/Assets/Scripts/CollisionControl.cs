bd7bd58ce5d14000911aec902b446ab887b2b8c5
Javier Ballón <javi4195@gmail.com>
2024-04-02 18:52:08 -0300

    Created Collision Control

---
+++ GamePong/Assets/Scripts/CollisionControl.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollisionControl : MonoBehaviour
+{
+    public MoveBall moveBall;
+
+    void CollisionWithRacket(Collision2D c)
+    {
+        Vector3 positionBall = this.transform.position;
+        Vector3 positionRacket = c.gameObject.transform.position;
+        float tallRacket = c.collider.bounds.size.y;
+
+        float x;
+
+        if(c.gameObject.name == "Racket1")
+        {
+            x = 1;
+        }
+        else
+        {
+            x = -1;
+        }
+
+        float y = (positionBall.y - positionRacket.y) / tallRacket;
+
+        this.moveBall.IncreaseHitCounter();
+        this.moveBall.MovementBall(new Vector2(x, y));
+
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.name == "Racket1" || collision.gameObject.name == "Racket2")
+        {
+            this.CollisionWithRacket(collision);
+        }
+        else if(collision.gameObject.name == "WallLeft")
+        {
+            print("Gol");
+        }
+        else if(collision.gameObject.name == "WallRight")
+        {
+            print("Gool");
+        }
+    }
+}
